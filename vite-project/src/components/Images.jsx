b7a894eeded47ce379f44d7b638ab068e87bf70e
SarangaLahiru <agslwijekoon@std.appsc.sab.ac.lk>
2024-07-29 18:25:48 +0530

    update2

--- vite-project/src/components/Images.jsx
+++ vite-project/src/components/Images.jsx
@@ -9,7 +9,7 @@ export function ImageCarousel() {
                 <img src="./img/Info Bash 23 FOC-595.jpg" alt="Slide 2" className="w-full h-full object-cover" />
                 <img src="./img/Info Bash 23 FOC-675.jpg" alt="Slide 3" className="w-full h-full object-cover" />
                 <img src="./img/Info Bash 23 FOC-698.jpg" alt="Slide 4" className="w-full h-full object-cover" />
-                <img src="./img/2023_09_01_18_07_IMG_2952.PNG" alt="Slide 5" className="w-full h-full object-cover" />
+                <img src="./img/2023_09_01_18_07_IMG_2952.jpg" alt="Slide 5" className="w-full h-full object-cover" />
             </Carousel>
         </div>
     );
