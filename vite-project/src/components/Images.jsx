7ac1231928e945188961695610012ced6e765b14
SarangaLahiru <agslwijekoon@std.appsc.sab.ac.lk>
2024-07-29 18:14:10 +0530

    update

--- vite-project/src/components/Images.jsx
+++ vite-project/src/components/Images.jsx
@@ -5,11 +5,11 @@ export function ImageCarousel() {
     return (
         <div className="relative w-full h-screen overflow-hidden">
             <Carousel>
-                <img src="./img/img (1).jpeg" alt="Slide 1" className="w-full h-full object-cover" />
-                <img src="./img/img (2).jpeg" alt="Slide 2" className="w-full h-full object-cover" />
-                <img src="./img/img (3).jpeg" alt="Slide 3" className="w-full h-full object-cover" />
-                <img src="./img/img (4).jpeg" alt="Slide 4" className="w-full h-full object-cover" />
-                <img src="./img/img (5).jpeg" alt="Slide 5" className="w-full h-full object-cover" />
+                <img src="./img/Info Bash 23 FOC-149.jpg" alt="Slide 1" className="w-full h-full object-cover" />
+                <img src="./img/Info Bash 23 FOC-595.jpg" alt="Slide 2" className="w-full h-full object-cover" />
+                <img src="./img/Info Bash 23 FOC-675.jpg" alt="Slide 3" className="w-full h-full object-cover" />
+                <img src="./img/Info Bash 23 FOC-698.jpg" alt="Slide 4" className="w-full h-full object-cover" />
+                <img src="./img/2023_09_01_18_07_IMG_2952.PNG" alt="Slide 5" className="w-full h-full object-cover" />
             </Carousel>
         </div>
     );
